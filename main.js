f71548ca34fe21d81e30abc443b5b250a59590c0
Evanflow89 <ivanbaragone@gmail.com>
2022-05-09 17:10:04 +0200

    commit

--- main.js
+++ main.js
@@ -29,9 +29,11 @@ console.log(CpuNumber);
 
 
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-
+// L’utente non può inserire più volte lo stesso numero.
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 
 let userBombs = [];
+let isGameOver = false;
 
 for (let i = 0; i < 84; i++) {
     const userChoose = Number(prompt("Inserisci un numero tra 1 e 100"));
@@ -46,8 +48,8 @@ for (let i = 0; i < 84; i++) {
     }
 }
 
-// L’utente non può inserire più volte lo stesso numero.
-// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+
+
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 // BONUS: (da fare solo se funziona tutto il resto)
